46142ee275fb87075dee6b28f58d38c4852b608d
Viktor Eikman <viktor.eikman@gmail.com>
2019-08-26 21:52:44 +0200

    First draft.

--- src/aiok_dupe/core.clj
+++ src/aiok_dupe/core.clj
@@ -1,7 +1,29 @@
 (ns aiok-dupe.core
-  (:gen-class))
+  (:require [clojure.string :as string]))
+
+(def ortho-sep #"\|")
+(def meaning-sep #",")
+
+(defn cluster
+  [kanji coll meaning]
+  (update coll (string/trim meaning) #(conj (or % []) kanji)))
+
+(defn organize
+  [coll line]
+  (let [[k mm] (string/split line ortho-sep)]
+    (if (nil? mm)
+      coll
+      (reduce (partial cluster k) coll (string/split mm meaning-sep)))))
+
+(defn by-count
+  [coll item]
+  (update coll (count (second item)) #(conj (or % {}) item)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Find duplicate proposed meanings in a kanji deck."
   [& args]
-  (println "Hello, World!"))
+  (let [lines (-> "resources/aiok.txt" slurp string/split-lines)
+        by-word (dissoc (reduce organize {} lines) "(kokuji)")
+        dupes-only (dissoc (reduce by-count {} by-word) 1)
+        sorted (reverse (sort dupes-only))]
+    (println (string/join "\n" sorted))))
